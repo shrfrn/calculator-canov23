bf227a3125060732243beea9bdec83b19dc6f4a4
shrfrn <shrfrn@gmail.com>
2024-01-29 20:29:36 +0200

    Add var for somthing

--- js/calculator.js
+++ js/calculator.js
@@ -4,6 +4,8 @@ var num1 = 0
 var num2 = 0
 var operator = ''
 
+var x = 10
+
 function onNumClick(elCell) {
     if(!num1) num1 = +elCell.innerText
     else if(operator && !num2) num2 = +elCell.innerText
